e32ce99678a44b7fcd5916777ec10307918c14b4
Mariska Adriaanzen <madriaanzen@gmail.com>
2025-04-06 13:58:02 +0200

    Use of tabs and assigning values from database to tools
    Makes use of tabs and shows how to assign values to things such as combo boxes from the database

---
+++ MDI and datagridviews/MDI and datagridviews/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace MDI_and_datagridviews
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.btnGoodMovies = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.dbView = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbxGenre = new System.Windows.Forms.ComboBox();
+            this.lstOutput = new System.Windows.Forms.ListBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dbView)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(3, 2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(999, 436);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dbView);
+            this.tabPage1.Controls.Add(this.btnExit);
+            this.tabPage1.Controls.Add(this.btnGoodMovies);
+            this.tabPage1.Controls.Add(this.btnDisplay);
+            this.tabPage1.Controls.Add(this.btnConnect);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(991, 407);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "All Movies";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.groupBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(991, 407);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Select Single Movie";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(793, 30);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(172, 37);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(793, 85);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(172, 37);
+            this.btnDisplay.TabIndex = 1;
+            this.btnDisplay.Text = "Dislay All";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // btnGoodMovies
+            // 
+            this.btnGoodMovies.Location = new System.Drawing.Point(793, 143);
+            this.btnGoodMovies.Name = "btnGoodMovies";
+            this.btnGoodMovies.Size = new System.Drawing.Size(172, 37);
+            this.btnGoodMovies.TabIndex = 2;
+            this.btnGoodMovies.Text = "Display Good Movies";
+            this.btnGoodMovies.UseVisualStyleBackColor = true;
+            this.btnGoodMovies.Click += new System.EventHandler(this.btnGoodMovies_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(857, 350);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(108, 37);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // dbView
+            // 
+            this.dbView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dbView.Location = new System.Drawing.Point(19, 18);
+            this.dbView.Name = "dbView";
+            this.dbView.RowHeadersWidth = 51;
+            this.dbView.RowTemplate.Height = 24;
+            this.dbView.Size = new System.Drawing.Size(720, 369);
+            this.dbView.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lstOutput);
+            this.groupBox1.Controls.Add(this.cmbxGenre);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(24, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(940, 350);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "View the Genre";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select a genre:";
+            // 
+            // cmbxGenre
+            // 
+            this.cmbxGenre.FormattingEnabled = true;
+            this.cmbxGenre.Location = new System.Drawing.Point(153, 33);
+            this.cmbxGenre.Name = "cmbxGenre";
+            this.cmbxGenre.Size = new System.Drawing.Size(121, 24);
+            this.cmbxGenre.TabIndex = 1;
+            this.cmbxGenre.SelectedIndexChanged += new System.EventHandler(this.cmbxGenre_SelectedIndexChanged);
+            // 
+            // lstOutput
+            // 
+            this.lstOutput.FormattingEnabled = true;
+            this.lstOutput.ItemHeight = 16;
+            this.lstOutput.Location = new System.Drawing.Point(153, 80);
+            this.lstOutput.Name = "lstOutput";
+            this.lstOutput.Size = new System.Drawing.Size(756, 244);
+            this.lstOutput.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1014, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dbView)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnGoodMovies;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.DataGridView dbView;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cmbxGenre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstOutput;
+    }
+}
+
---
+++ MDI and datagridviews/MDI and datagridviews/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace MDI_and_datagridviews
+{
+    public partial class Form1 : Form
+    {
+
+        string connStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Film.mdf;Integrated Security=True";
+        SqlConnection conn;
+        SqlCommand comm;
+
+        SqlDataAdapter adapter;
+        DataSet ds;
+
+        SqlDataReader reader;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn = new SqlConnection(connStr);
+
+                conn.Open();
+
+                string sql = "SELECT DISTINCT Genre FROM Movie";
+
+                comm = new SqlCommand(sql, conn);
+                reader = comm.ExecuteReader();
+
+                cmbxGenre.Items.Clear();
+
+                while (reader.Read())
+                {
+                    cmbxGenre.Items.Add(reader.GetValue(0));
+                }
+
+                MessageBox.Show("This Connection works!");
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Oh goodness here we go: " + ex);
+                return;
+            }
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+
+                adapter = new SqlDataAdapter();
+                ds = new DataSet();
+
+                string sql = "SELECT * FROM Movie";
+
+                comm = new SqlCommand(sql, conn);
+
+                adapter.SelectCommand = comm;
+                adapter.Fill(ds, "Movie");
+
+                dbView.DataSource = ds;
+                dbView.DataMember = "Movie";                
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("How did you not do this: " + ex);
+                return;
+            }
+        }
+
+        private void btnGoodMovies_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+
+                adapter = new SqlDataAdapter();
+                ds = new DataSet();
+
+                string sql = "SELECT * FROM Movie WHERE Rating > 3";
+
+                comm = new SqlCommand(sql, conn);
+
+                adapter.SelectCommand = comm;
+                adapter.Fill(ds, "Movie");
+
+                dbView.DataSource = ds;
+                dbView.DataMember = "Movie";
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("How did you not do this: " + ex);
+                return;
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void cmbxGenre_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+
+                string sql = "SELECT MovieID, MovieName, Rating, Price  FROM Movie WHERE Genre LIKE '%" + cmbxGenre.SelectedItem + "%'";
+
+                comm = new SqlCommand(sql, conn);
+                reader = comm.ExecuteReader();
+
+                lstOutput.Items.Clear();
+
+                lstOutput.Items.Add("ID\tMovie Name\tRating\tPrice");
+                lstOutput.Items.Add("-----------------------------------------------------------------");
+
+                while (reader.Read())
+                {
+                    lstOutput.Items.Add(reader.GetValue(0) + "\t" + reader.GetValue(1) + "\t" + reader.GetValue(2) + "\t" + reader.GetValue(3) + "\t");
+                }
+
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Almost got it: " + ex);
+                return;
+            }
+        }
+    }
+}
