2fb7d55844c690bdaa0827a703f191e7051d40ac
Mariska Adriaanzen <madriaanzen@gmail.com>
2025-06-09 17:24:44 +0200

    took a whiule but figured it out

---
+++ Postback/Postback/WebForm1.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Postback.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Font-Bold="True" Font-Size="Large" Text="Concert Booking"></asp:Label>
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Name and Surname:"></asp:Label>
+            <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="Label4" runat="server" Text="Number of tickets:"></asp:Label>
+            <br />
+            <asp:RadioButton ID="rdoOne" runat="server" GroupName="Ticket amount" Text="1" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:RadioButton ID="rdoTwo" runat="server" GroupName="Ticket amount" Text="2" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:RadioButton ID="rdoThree" runat="server" GroupName="Ticket amount" Text="3" />
+            <br />
+            <asp:Label ID="Label5" runat="server" Text="Select show tjme:"></asp:Label>
+            <br />
+            <asp:Button ID="btnMorning" runat="server" CausesValidation="False" OnClick="btnMorning_Click" Text="11:00" />
+&nbsp;<asp:Button ID="btnMidday" runat="server" CausesValidation="False" OnClick="btnMidday_Click" Text="14:00" />
+&nbsp;<asp:Button ID="btnEvening" runat="server" CausesValidation="False" OnClick="btnEvening_Click" Text="17:00" />
+            <br />
+            <br />
+            <br />
+            <asp:Button ID="btnBook" runat="server" Height="50px" OnClick="btnBook_Click" Text="Book" Width="354px" />
+            <br />
+            <asp:Label ID="lblDisplay" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Postback/Postback/WebForm1.aspx.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Postback
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        protected void btnBook_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            int tickets = 0;
+            string time;
+
+            if (rdoOne.Checked)
+            {
+                tickets = 1;
+            }
+            else if (rdoTwo.Checked)
+            {
+                tickets = 2;
+            }
+            else if (rdoThree.Checked)
+            {
+                tickets = 3;
+            }
+            else
+            { 
+                lblDisplay.Text = "Please select the number of tickets.";
+                return;
+            }
+
+            if (btnMorning.Enabled)
+            {
+                time = btnMorning.Text;
+            }
+            else if (btnMidday.Enabled)
+            {
+                time = btnMidday.Text;
+            }
+            else if (btnEvening.Enabled)
+            {
+                time = btnEvening.Text;
+            }
+            else
+            {
+                lblDisplay.Text = "Please select a show time.";
+                return;
+            }
+
+            lblDisplay.Text = name + "you have booked the show for " + tickets + " people at " + time;
+
+            btnMorning.Enabled = true;
+            btnMidday.Enabled = true;   
+            btnEvening.Enabled = true;
+        }
+
+
+        //CausesValidation property for all 3 buttons are setto false
+        protected void btnMorning_Click(object sender, EventArgs e)
+        {
+           btnMorning.Enabled = true;
+            btnMidday.Enabled = false;
+            btnEvening.Enabled = false;
+        }
+
+        protected void btnMidday_Click(object sender, EventArgs e)
+        {
+            btnMorning.Enabled = false;
+            btnMidday.Enabled = true;
+            btnEvening.Enabled = false;
+        }
+
+        protected void btnEvening_Click(object sender, EventArgs e)
+        {
+            btnMorning.Enabled = false;
+            btnMidday.Enabled = false;
+            btnEvening.Enabled = true;
+        }
+    }
+}
