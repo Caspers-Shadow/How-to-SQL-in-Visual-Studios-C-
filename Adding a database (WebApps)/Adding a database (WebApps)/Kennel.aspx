78d1fab9b6495fe4b3b2d51f417e6950ef563b12
Mariska Adriaanzen <madriaanzen@gmail.com>
2025-06-10 12:46:49 +0200

    pets baby

---
+++ Adding a database (WebApps)/Adding a database (WebApps)/Kennel.aspx
@@ -0,0 +1,61 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Kennel.aspx.cs" Inherits="Adding_a_database__WebApps_.Kennel" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            height: 48px;
+        }
+    </style>
+</head>
+<body style="background-color: #66CCFF">
+    <form id="form1" runat="server">
+        <div>
+            <table style="width:100%;">
+                <tr>
+                    <td>
+                        <asp:Label ID="Label1" runat="server" Font-Size="XX-Large" ForeColor="#CC33FF" Text="Happy Pet Kennel"></asp:Label>
+                    </td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td rowspan="3">
+                        <asp:GridView ID="gvView" runat="server" BackColor="#33CC33" ForeColor="White">
+                        </asp:GridView>
+                    </td>
+                    <td>
+                        <asp:Button ID="btnAll" runat="server" Font-Bold="True" OnClick="btnAll_Click" Text="Show all the pets" />
+                    </td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td class="auto-style1">
+                        <asp:Button ID="btnLarge" runat="server" Font-Bold="True" OnClick="btnLarge_Click" Text="Show all large pets" />
+                    </td>
+                    <td class="auto-style1"></td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="lblError" runat="server"></asp:Label>
+                    </td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Adding a database (WebApps)/Adding a database (WebApps)/Kennel.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Adding_a_database__WebApps_
+{
+    public partial class Kennel : System.Web.UI.Page
+    {
+        string conStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Kennel.mdf;Integrated Security=True";
+        SqlConnection conn;
+        SqlCommand comm;
+        SqlDataAdapter adapter;
+        DataSet ds;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+                       
+        }
+
+        protected void btnAll_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn = new SqlConnection(conStr);
+                conn.Open();
+
+                adapter = new SqlDataAdapter();
+                ds = new DataSet();
+
+                string sql = "SELECT HouseID, PetName, PetType FROM PetInfo";
+
+                comm = new SqlCommand(sql, conn);
+
+                adapter.SelectCommand = comm;
+                adapter.Fill(ds);
+
+                gvView.DataSource = ds;
+                gvView.DataBind();
+            }
+
+            catch (Exception ex)
+            {
+                gvView.Visible = false;
+                lblError.Text = "Error: " + ex.Message;
+                return;
+            }
+        }
+
+        protected void btnLarge_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn = new SqlConnection(conStr);
+                conn.Open();
+
+                adapter = new SqlDataAdapter();
+                ds = new DataSet();
+
+                string sql = "SELECT HouseID, PetName FROM PetInfo WHERE PetSize LIKE '%Large%'";
+
+                comm = new SqlCommand(sql, conn);
+
+                adapter.SelectCommand = comm;
+                adapter.Fill(ds);
+
+                gvView.DataSource = ds;
+                gvView.DataBind();
+            }
+
+            catch (Exception ex)
+            {
+                gvView.Visible = false;
+                lblError.Text = "Error: " + ex.Message;
+                return;
+            }
+        }
+    }
+}
